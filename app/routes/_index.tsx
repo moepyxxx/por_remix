015cf264e5869ce8868a738d44e7be2efa8bc968
moeko-iwai <moepyxxxxxx@gmail.com>
2024-12-20 16:18:55 +0900

    update: vdh

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,7 +46,7 @@ export default function Index() {
   }, []);
 
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-dvh items-center justify-center">
       {Array.from({ length: fluffyCount }, (_, i) => i).map((i) => {
         return (
           <Fluffy
